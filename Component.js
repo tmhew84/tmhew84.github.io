57a4eaeb9457ccf74ce2689bcb2cdbb900a0f166
tmhew84 <tmhew84@gmail.com>
2015-05-17 23:56:09 -0500

    initial setup

---
+++ Component.js
@@ -0,0 +1,15 @@
+sap.ui.define(
+	[
+		'sap/ui/core/UIComponent'
+	],
+
+	function (UIComponent) {
+		'use strict';
+
+		var Component = UIComponent.extend('com.tm.Component', {
+			metadata: 'json'
+		});
+
+		return Component;
+	}
+);
---
+++ view/App.controller.js
@@ -0,0 +1,13 @@
+sap.ui.define(
+	[
+		'sap/ui/core/mvc/Controller'
+	],
+
+	function (CoreController) {
+		'use strict';
+
+		var Controller = CoreController.extend('com.tm.view.App');
+
+		return Controller;
+	}
+);
