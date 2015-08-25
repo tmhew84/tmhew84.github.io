012702aa590001f1f563bc4ae7b741e9fc3e4ed8
tmhew84 <tmhew84@gmail.com>
2015-08-24 22:54:37 -0500

    table

--- com/ui5/md/controller/App.controller.js
+++ com/ui5/md/controller/App.controller.js
@@ -10,10 +10,6 @@ sap.ui.define(
 
         var CustomController = Controller.extend('com.ui5.md.controller.App');
 
-        CustomController.prototype.onInit = function () {
-            console.log(this.getOwnerComponent().getModel('comp.northwind'));
-        };
-
         return CustomController;
     }
 );
