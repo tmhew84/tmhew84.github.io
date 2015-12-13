f49d797069ffd0f8de605ac22f5ba3a9db72ba76
tmhew84 <tmhew84@gmail.com>
2015-12-12 23:35:42 -0600

    index

---
+++ controller/Index.controller.js
@@ -0,0 +1,9 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], function (Controller) {
+    "use strict";
+
+    var CustomController = Controller.extend("com.tm.controller.Index");
+
+    return CustomController;
+});
