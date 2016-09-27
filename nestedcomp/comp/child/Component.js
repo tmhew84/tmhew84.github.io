f39ebc772da440d6026084f966704ec9f5ebe5cf
tmhew <tmhew84@gmail.com>
2016-09-27 02:18:26 -0400

    nest

---
+++ nestedcomp/comp/child/Component.js
@@ -0,0 +1,14 @@
+sap.ui.define([
+    'sap/ui/core/UIComponent'
+], (UIComponent) => {
+   'use strict';
+
+    const CustomComponent = UIComponent.extend('nest.comp.child.Component');
+
+    CustomComponent.prototype.init = function () {
+        UIComponent.prototype.init.apply(this);
+        this.getRouter().initialize();
+    };
+
+    return CustomComponent;
+});
---
+++ nestedcomp/controller/Parent.controller.js
@@ -0,0 +1,25 @@
+sap.ui.define([
+    'sap/ui/core/mvc/Controller',
+    'sap/ui/core/ComponentContainer'
+], (Controller, ComponentContainer) => {
+    'use strict';
+
+    const CustomController = Controller.extend('nest.controller.Parent');
+
+    CustomController.prototype.onInit = function () {
+        sap.ui.component({
+            name: 'nest.component.child',
+            manifestFirst: true,
+            async: true
+        }).then(oComponent => {
+            this.getView().byId('page').addContent(
+                new ComponentContainer({
+                    height: '100%',
+                    component: oComponent
+                })
+            );
+        });
+    };
+
+    return CustomController;
+});
