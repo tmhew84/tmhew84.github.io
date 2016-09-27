d5535c3a4ea9042256d9c6a81a96c016a01d6f77
tmhew <tmhew84@gmail.com>
2016-09-27 02:17:53 -0400

    cleanup

--- controller/Index.controller.js
+++
@@ -1,9 +0,0 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function (Controller) {
-    "use strict";
-
-    var CustomController = Controller.extend("com.tm.controller.Index");
-
-    return CustomController;
-});
